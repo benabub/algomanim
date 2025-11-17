188875193dd91b38b1a9c0e13e08e2fa7bdfb015
Benedict Abub <fkhurkhd2123dfs@tutamail.com>
2025-11-18 04:26:14 +0500

    Change AlgoManimBase: position() -> _position() + 2 attrs removed; Add NodeDataStructure(VisualDataStructure); Add LinkedList(NodeDataStructure); Add LinkedList:  create_linked_list(),  get_linked_list_length(),  linked_list_to_list(),  _create_containers_mob(), _position(),  _create_and_pos_arrows_mob(),  _create_frame_mob(),  _rotate_frame(),  _text_config(),  _create_and_pos_values_mob(), update_value()

--- src/algomanim/algomanim.py
+++ src/algomanim/algomanim.py
@@ -11,11 +11,24 @@ import manim as mn
 from manim import ManimColor
 import numpy as np
 
-# from . import utils
+
+from .datastructures import (
+    #     Node,
+    #     TreeNode,
+    ListNode,
+)
+from .svg import SVG_DIR
 
 
 class AlgoManimBase(mn.VGroup):
     """Base class for all algomanim classes"""
+
+    def __init__(self):
+        super().__init__()
+        # ---- position ----
+        self._vector: np.ndarray = mn.ORIGIN
+        self._mob_center: mn.Mobject = mn.Dot(mn.ORIGIN)
+        self._align_edge: Literal["up", "down", "left", "right"] | None = None
 
     def first_appear(self, scene: mn.Scene, time=0.5):
         """Animate the initial appearance in scene.
@@ -34,12 +47,10 @@ class AlgoManimBase(mn.VGroup):
         """
         scene.add(self)
 
-    def position(
+    def _position(
         self,
         mobject: mn.Mobject,
-        mob_center: mn.Mobject,
-        align_edge: Literal["up", "down", "left", "right"] | None,
-        vector: np.ndarray,
+        mob_center: mn.Mobject = mn.Dot(mn.ORIGIN),
     ) -> None:
         """Position mobject relative to center with optional edge alignment.
 
@@ -49,25 +60,25 @@ class AlgoManimBase(mn.VGroup):
             align_edge: Which edge to align to (None for center)
             vector: Additional offset vector
         """
-        if align_edge:
-            if align_edge in ["UP", "up"]:
+        if self._align_edge:
+            if self._align_edge in ["UP", "up"]:
                 mobject.move_to(mob_center.get_center())
                 mobject.align_to(mob_center, mn.UP)
-                mobject.shift(vector)
-            elif align_edge in ["DOWN", "down"]:
+                mobject.shift(self._vector)
+            elif self._align_edge in ["DOWN", "down"]:
                 mobject.move_to(mob_center.get_center())
                 mobject.align_to(mob_center, mn.DOWN)
-                mobject.shift(vector)
-            elif align_edge in ["RIGHT", "right"]:
+                mobject.shift(self._vector)
+            elif self._align_edge in ["RIGHT", "right"]:
                 mobject.move_to(mob_center.get_center())
                 mobject.align_to(mob_center, mn.RIGHT)
-                mobject.shift(vector)
-            elif align_edge in ["LEFT", "left"]:
+                mobject.shift(self._vector)
+            elif self._align_edge in ["LEFT", "left"]:
                 mobject.move_to(mob_center.get_center())
                 mobject.align_to(mob_center, mn.LEFT)
-                mobject.shift(vector)
+                mobject.shift(self._vector)
         else:
-            mobject.move_to(mob_center.get_center() + vector)
+            mobject.move_to(mob_center.get_center() + self._vector)
 
 
 class VisualDataStructure(AlgoManimBase):
@@ -643,6 +654,16 @@ class RectangleCellsDataStructure(VisualDataStructure):
             self._deep_bottom_buff = deep_bottom_buff
 
 
+class NodeDataStructure(VisualDataStructure):
+    """
+    ...
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._radius: float = 0.4
+
+
 class Array(RectangleCellsDataStructure):
     """Array visualization as a VGroup of cells with values and pointers.
 
@@ -743,12 +764,7 @@ class Array(RectangleCellsDataStructure):
         self._containers_mob.arrange(mn.RIGHT, buff=0.1)
 
         # move VGroup to the specified position
-        self.position(
-            self._containers_mob,
-            self._mob_center,
-            self._align_edge,
-            self._vector,
-        )
+        self._position(self._containers_mob, self._mob_center)
 
         # move text mobjects in containers
         self._position_values_in_containers()
@@ -782,7 +798,7 @@ class Array(RectangleCellsDataStructure):
             fill_color=self._fill_color,
             fill_opacity=1.0,
         )
-        self.position(containers_mob, self._mob_center, self._align_edge, self._vector)
+        self._position(containers_mob, self._mob_center)
         empty_value_mob.move_to(containers_mob.get_center())
         empty_value_mob.align_to(containers_mob, mn.DOWN)
         empty_value_mob.align_to(containers_mob, mn.LEFT)
@@ -1131,12 +1147,7 @@ class String(RectangleCellsDataStructure):
         self._letters_cells_left_edge = self._containers_mob.get_left()
 
         # move letters cells to the specified position
-        self.position(
-            self._containers_mob,
-            self._mob_center,
-            self._align_edge,
-            self._vector,
-        )
+        self._position(self._containers_mob, self._mob_center)
 
         self._left_quote_cell_mob, self._right_quote_cell_mob = (
             self._create_and_pos_quote_cell_mobs()
@@ -1205,7 +1216,7 @@ class String(RectangleCellsDataStructure):
 
         empty_value_mob = mn.Text('""', **self._text_config())
         containers_mob = mn.Square(**self._containers_cell_config())
-        self.position(containers_mob, self._mob_center, self._align_edge, self._vector)
+        self._position(containers_mob, self._mob_center)
         empty_value_mob.next_to(
             containers_mob.get_top(),
             direction=mn.DOWN,
@@ -1271,22 +1282,18 @@ class String(RectangleCellsDataStructure):
     def get_containers_mob(self):
         return self._containers_mob
 
-    def position(
+    def _position(
         self,
         mobject: mn.Mobject,
-        mob_center: mn.Mobject,
-        align_edge: Literal["up", "down", "left", "right"] | None,
-        vector: np.ndarray,
+        mob_center: mn.Mobject = mn.Dot(mn.ORIGIN),
     ) -> None:
         """
         ...
         """
         if isinstance(mob_center, String):
-            AlgoManimBase.position(
-                self, mobject, mob_center.get_containers_mob(), align_edge, vector
-            )
+            AlgoManimBase._position(self, mobject, mob_center.get_containers_mob())
         else:
-            AlgoManimBase.position(self, mobject, mob_center, align_edge, vector)
+            AlgoManimBase._position(self, mobject, mob_center)
 
     def update_value(
         self,
@@ -1369,6 +1376,328 @@ class String(RectangleCellsDataStructure):
             scene.add(self)
 
 
+class LinkedList(NodeDataStructure):
+    """
+    ...
+    """
+
+    def __init__(
+        self,
+        head: ListNode | None,
+        # head: list,
+        radius: float = 0.4,
+        direction: np.ndarray = mn.RIGHT,
+        # --- containers colors ---
+        node_color: ManimColor | str = mn.BLACK,
+        fill_color: ManimColor | str = mn.LIGHT_GRAY,
+        bg_color: ManimColor | str = mn.DARK_GRAY,
+        # -- position --
+        vector: np.ndarray = mn.ORIGIN,
+        mob_center: mn.Mobject = mn.Dot(mn.ORIGIN),
+        align_edge: Literal["up", "down", "left", "right"] | None = None,
+        # -- font --
+        font="",
+        font_color: ManimColor | str = mn.BLACK,
+        weight: str = "NORMAL",
+    ):
+        super().__init__()
+        # self._data = head
+        self._data = self.linked_list_to_list(head)  # save head as list
+        self._radius = radius
+        self._direction = direction
+        self._container_color = node_color
+        self._fill_color = fill_color
+        self._bg_color = bg_color
+        self._vector = vector
+        self._mob_center = mob_center
+        self._align_edge = align_edge
+        self._font = font
+        self._font_color = font_color
+        self._weight = weight
+
+        # nodes
+        self._containers_mob = self._create_containers_mob()
+
+        # arrows
+        self._arrows_mob = self._create_and_pos_arrows_mob()
+
+        # pointers
+        self._pointers_top, self._pointers_bottom = self.create_pointers(
+            self._containers_mob
+        )
+
+        # group all mobs
+        self._frame_mob = self._create_frame_mob()
+
+        # rotate frame
+        self._rotate_frame()
+
+        self._position(self._frame_mob, self._mob_center)
+
+        # values
+        self._values_mob = self._create_and_pos_values_mob()
+
+        # ---- add ------
+        self.add(self._frame_mob, self._values_mob)
+
+    @staticmethod
+    def create_linked_list(value: list) -> ListNode | None:
+        """Create a singly-linked list from a list.
+
+        Args:
+            value: List to convert into linked list nodes.
+
+        Returns:
+            Head node of the created linked list, or None if values is empty.
+        """
+
+        if not value:
+            return None
+        head = ListNode(value[0])
+        current = head
+        for val in value[1:]:
+            current.next = ListNode(val)
+            current = current.next
+        return head
+
+    @staticmethod
+    def get_linked_list_length(head: ListNode | None) -> int:
+        """Calculate the length of a linked list.
+
+        Args:
+            head: Head node of the linked list.
+
+        Returns:
+            Number of nodes in the linked list. 0 if head is None.
+        """
+        count = 0
+        current = head
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    @staticmethod
+    def linked_list_to_list(head: ListNode | None) -> list:
+        """Convert a linked list to a Python list.
+
+        Args:
+            head: Head node of the linked list.
+
+        Returns:
+            List containing all values from the linked list in order.
+            Empty list if head is None.
+        """
+        result = []
+        current = head
+        while current:
+            result.append(current.val)
+            current = current.next
+        return result
+
+    def _create_containers_mob(self):
+        """
+        ...
+        """
+        node = mn.Circle(
+            radius=self._radius,
+            color=self._container_color,
+            fill_color=self._fill_color,
+            fill_opacity=1,
+            stroke_width=self._radius * 7,
+        )
+        containers_mob = mn.VGroup(*[node.copy() for _ in range(len(self._data))])
+        containers_mob.arrange(buff=self._radius)
+
+        return containers_mob
+
+    def _position(
+        self,
+        mobject: mn.Mobject,
+        mob_center: mn.Mobject = mn.Dot(mn.ORIGIN),
+    ) -> None:
+        """
+        ...
+        """
+        first_node_center = self._containers_mob[0].get_center()
+
+        target_point = self._mob_center.get_center() + self._vector
+
+        if self._align_edge:
+            if self._align_edge in ["LEFT", "left"]:
+                target_point = (
+                    mob_center.get_left() + self._vector + mn.RIGHT * self._radius
+                )
+            elif self._align_edge in ["RIGHT", "right"]:
+                target_point = (
+                    mob_center.get_right() + self._vector + mn.LEFT * self._radius
+                )
+            elif self._align_edge in ["UP", "up"]:
+                target_point = (
+                    mob_center.get_top() + self._vector + mn.DOWN * self._radius
+                )
+            elif self._align_edge in ["DOWN", "down"]:
+                target_point = (
+                    mob_center.get_bottom() + self._vector + mn.UP * self._radius
+                )
+
+        shift_vector = target_point - first_node_center
+        mobject.shift(shift_vector)
+
+    def _create_and_pos_arrows_mob(self):
+        """
+        ...
+        """
+
+        arrow_path = str(SVG_DIR / "arrows/radius_x10.svg")
+        arrow = mn.SVGMobject(
+            arrow_path,
+            width=self._radius,
+        )
+        arrows_mob = mn.VGroup()
+        for i in range(len(self._data) - 1):
+            new_arrow = arrow.copy()
+            new_arrow.next_to(self._containers_mob[i], buff=0)
+            arrows_mob.add(new_arrow)
+        return arrows_mob
+
+    def _create_frame_mob(self):
+        return mn.VGroup(
+            self._containers_mob,
+            self._arrows_mob,
+            self._pointers_top,
+            self._pointers_bottom,
+        )
+
+    def _rotate_frame(self) -> None:
+        """
+        ...
+        """
+
+        if not np.allclose(self._direction, mn.RIGHT):
+            angle = mn.angle_of_vector(self._direction)
+            self._frame_mob.rotate(
+                angle,
+                about_point=self._containers_mob[0].get_center(),
+            )
+
+    def _text_config(self):
+        return {
+            "font": self._font,
+            "weight": self._weight,
+            "color": self._font_color,
+        }
+
+    def _create_and_pos_values_mob(self):
+        """
+        ...
+        """
+
+        top_bottom_buff = self._radius / 2
+        ypgj_shift = self._radius / 16
+        max_size_test = (self._radius - top_bottom_buff) * 2
+        max_size_center = (self._radius - top_bottom_buff) * 2.5
+        max_size_shift = (self._radius - top_bottom_buff) * 2.2
+
+        # find base font_size
+        font_size = 10
+        test_mob = mn.Text("0", font_size=font_size)
+        while test_mob.height < max_size_test:
+            font_size += 1
+            test_mob = mn.Text("0", font_size=font_size)
+
+        values_mob = mn.VGroup(
+            *[
+                mn.Text(str(val), font_size=font_size, **self._text_config())
+                for val in self._data
+            ]
+        )
+
+        for i in range(len(self._data)):
+            val = str(self._data[i])
+            val_set = set(val)
+            mob = values_mob[i]
+            width = mob.width
+            container = self._containers_mob[i]
+
+            if len(val) == 1 and val in "0123456789":  # center alignment
+                mob.move_to(container)
+                continue
+
+            if val_set.issubset({'"', "'", "^", "`"}):  # top alignment
+                if width > max_size_shift:
+                    mob.scale_to_fit_width(max_size_shift)
+                mob.next_to(
+                    container.get_top(),
+                    direction=mn.DOWN,
+                    buff=top_bottom_buff,
+                )
+                continue
+
+            if val_set.issubset({"y", "p", "g", "j"}):  # down shift
+                if width > max_size_center:
+                    mob.scale_to_fit_width(max_size_center)
+                mob.move_to(container)
+                mob.shift(mn.DOWN * ypgj_shift)
+                continue
+
+            if val_set.issubset({".", ",", "_"}):  # bottom alignment
+                if width > max_size_shift:
+                    mob.scale_to_fit_width(max_size_shift)
+                mob.next_to(
+                    container.get_bottom(),
+                    direction=mn.UP,
+                    buff=top_bottom_buff,
+                )
+                continue
+
+            if width > max_size_center:
+                mob.scale_to_fit_width(max_size_center)
+            mob.move_to(container)
+
+        return values_mob
+
+    def update_value(
+        self,
+        scene: mn.Scene,
+        new_value,
+        animate: bool = False,
+        run_time: float = 0.2,
+    ) -> None:
+        """
+        ...
+        """
+
+        # checks
+        if not self._data and not new_value:
+            return
+
+        # save old group status
+        highlight_status = self._save_highlights_states()
+
+        # new group
+        new_group = LinkedList(
+            new_value,
+            font=self._font,
+            direction=self._direction,
+            mob_center=self._mob_center,
+            align_edge=self._align_edge,
+            vector=self._vector,
+        )
+
+        # restore colors
+        self._preserve_highlights_states(new_group, highlight_status)
+
+        # add
+        if animate:
+            scene.play(mn.Transform(self, new_group), run_time=run_time)
+            self._update_internal_state(new_value, new_group)
+        else:
+            scene.remove(self)
+            self._update_internal_state(new_value, new_group)
+            scene.add(self)
+
+
 class RelativeTextValue(AlgoManimBase):
     """Text group showing scope variables positioned relative to mobject.
 
@@ -1423,7 +1752,7 @@ class RelativeTextValue(AlgoManimBase):
         )
 
         # construction: Move VGroup to the specified position
-        self.position(text_mob, mob_center, align_edge, vector)
+        self._position(text_mob, mob_center)
 
         self.add(*text_mob)
 
@@ -1492,6 +1821,9 @@ class RelativeText(AlgoManimBase):
     ):
         super().__init__()
 
+        self._vector = vector
+        self._align_edge = align_edge
+
         text_mob = mn.Text(
             text,
             font=font,
@@ -1501,7 +1833,7 @@ class RelativeText(AlgoManimBase):
         )
 
         # construction: Move VGroup to the specified position
-        self.position(text_mob, mob_center, align_edge, vector)
+        self._position(text_mob, mob_center)
 
         self.add(text_mob)
 
@@ -1546,6 +1878,8 @@ class CodeBlock(AlgoManimBase):
         self.font_color_regular = font_color_regular
         self.font_color_highlight = font_color_highlight
         self.bg_highlight_color = bg_highlight_color
+        self._vector = vector
+        self._align_edge = align_edge
 
         self.code_mobs = [
             mn.Text(line, font=font, font_size=font_size, color=self.font_color_regular)
@@ -1582,7 +1916,7 @@ class CodeBlock(AlgoManimBase):
             block_vgroup = code_vgroup
 
         # construction: Move VGroup to the specified position
-        self.position(block_vgroup, mob_center, align_edge, vector)
+        self._position(block_vgroup, mob_center)
 
         self.add(block_vgroup)
 
@@ -1677,6 +2011,8 @@ class TitleText(AlgoManimBase):
         **kwargs,
     ):
         super().__init__()
+        self._vector = vector
+        self._align_edge = align_edge
 
         # create the text mobject
         text_mobject = mn.Text(
@@ -1687,7 +2023,7 @@ class TitleText(AlgoManimBase):
             **kwargs,
         )
 
-        self.position(text_mobject, mob_center, align_edge, vector)
+        self._position(text_mobject, mob_center)
 
         self.add(text_mobject)
 
@@ -1833,6 +2169,8 @@ class TitleLogo(AlgoManimBase):
         **kwargs,
     ):
         super().__init__()
+        self._vector = vector
+        self._align_edge = align_edge
 
         # create the svg mobject
         self.svg = mn.SVGMobject(
@@ -1842,7 +2180,7 @@ class TitleLogo(AlgoManimBase):
         )
 
         # position the entire group relative to the reference mobject and offset vector
-        self.position(self.svg, mob_center, align_edge, vector)
+        self._position(self.svg, mob_center)
 
         self.add(self.svg)
 
